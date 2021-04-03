4d5fecc592861ab9b8f2605168a9a77bd3f90c80
gabrielomelo <dev.gabrielomelo@gmail.com>
2021-04-03 14:37:39 -0300

    tá tudo certo aqui

--- lstm_convert_vectorized_dataset.py
+++ lstm_convert_vectorized_dataset.py
@@ -1,10 +1,10 @@
 import pickle
 import pandas as pd
-import numpy as np
 from tqdm import tqdm
 
 dataset_path = '../../vectorized.p'
 converted_dataset_path = '../lstm_data/converted_vectorized_twitter_100_stemmed.p'
+converted_dataset_labels_path = '../lstm_data/converted_labels_twitter_100_stemmed.p'
 dataset = pd.read_pickle(dataset_path)
 
 xs, ys = [], []
@@ -17,8 +17,8 @@ for line in tqdm(dataset['user']):
         xs.append(temp)
         ys.append(float(line['hasDepression']))
 
-converted_dataset = {'xs': np.array(xs, dtype=list), 'ys': np.array(ys, dtype=list)}
-
-print('gravando essa porra')
 with open(converted_dataset_path, 'wb+') as fp:
-    pickle.dump(converted_dataset, fp)
+    pickle.dump(xs, fp)
+
+with open(converted_dataset_labels_path, 'wb+') as fp:
+    pickle.dump(ys, fp)
--- lstm_dataset_preparation.py
+++ lstm_dataset_preparation.py
@@ -4,19 +4,20 @@ import numpy as np
 from tqdm import tqdm
 from svm.classifier import SVMClassifier
 from svm.vectorizer import SVMVectorizer
+from sys import getsizeof
 
 dataset_path = r'../../unvectorized.p'
 classifier_path = r'../fitted_classifiers/svm_classifier_100_stemmed.p'
 vectorizer_path = r'../fitted_classifiers/svm_vectorizer_100_stemmed.p'
 
 vectorized_dataset_path = r'../lstm_data/converted_vectorized_twitter_100_stemmed.p'
-new_classification_dataset_path = '../lstm_data/sentiment_classification_twitter_100_stemmed.p'
-new_concat_dataset_path = '../lstm_data/concat_twitter_dataset_100_stemmed.p'
+new_classification_dataset_path = r'../lstm_data/sentiment_classification_twitter_100_stemmed.p'
+new_concat_dataset_path = r'../lstm_data/concat_twitter_dataset_100_stemmed.p'
 vectorized_dataset = pd.read_pickle(vectorized_dataset_path)
 
 
 def vectorize_corpus(_vectorizer: SVMVectorizer, _classifier: SVMClassifier, corpus: list):
-    _temp = []
+    _temp, _temp2 = [], []
     bow = _vectorizer.vectorizer.transform(corpus)
     truncated_bow = _vectorizer.svd.transform(bow)
     scaled_bow = _vectorizer.scaler.transform(truncated_bow)
@@ -25,30 +26,34 @@ def vectorize_corpus(_vectorizer: SVMVectorizer, _classifier: SVMClassifier, cor
 
     for classification in classifications:
         if classification == 0:
-            _temp.append(np.array([1.0, 0.0], dtype=np.float32))  # negativo
+            _temp.append([0])  # sentimento negativo
+            _temp2.append(np.array([0, 1], dtype=np.float32))
         elif classification == 4:
-            _temp.append(np.array([0.0, 1.0], dtype=np.float32))  # positivo
-    return _temp
+            _temp.append([1])  # sentimento positivo
+            _temp2.append(np.array([1, 0], dtype=np.float32))
+    return _temp, _temp2
 
 
 dataset = pd.read_pickle(dataset_path)
 vectorizer = pd.read_pickle(vectorizer_path)
 classifier = pd.read_pickle(classifier_path)
-classified_dataset = []
+classified_dataset, classified_dataset2 = [], []
 
 for line in tqdm(dataset['user']):
     if len(line['tweets']) > 0:
         temp = []
         for tweet in line['tweets']:
             temp.append(tweet['content'])
-        classified_dataset.append(vectorize_corpus(vectorizer, classifier, temp))
+        single_dim, dual_dim = vectorize_corpus(vectorizer, classifier, temp)
+        classified_dataset.append(dual_dim)
+        classified_dataset2.append(single_dim)
 
 with open(new_classification_dataset_path, 'wb+') as fp:
     pickle.dump(classified_dataset, fp)
 
-for i in tqdm(range(0, len(vectorized_dataset['xs']))):
-    for j in range(0, len(vectorized_dataset['xs'][i])):
-        vectorized_dataset['xs'][i][j] = np.append(vectorized_dataset['xs'][i][j], classified_dataset[i][j])
+for i in tqdm(range(0, len(vectorized_dataset))):
+    for j in range(0, len(vectorized_dataset[i])):
+        vectorized_dataset[i][j] = np.append(vectorized_dataset[i][j], classified_dataset2[i][j])
 
 with open(new_concat_dataset_path, 'wb+') as fp:
     pickle.dump(vectorized_dataset, fp)
